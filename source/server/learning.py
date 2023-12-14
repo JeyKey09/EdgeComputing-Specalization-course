e7d59701487565b1bc334e46efff43483d9a5c1b
HHogasen <hallgeirhogasen@hotmail.com>
2023-12-14 22:32:15 +0100

    modell oppdatering

--- source/server/learning.py
+++ source/server/learning.py
@@ -1,47 +1,68 @@
 import tensorflow as tf
 from keras.models import Sequential
 from keras.datasets import cifar10
-from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D, InputLayer
+from keras.layers import Dense, Dropout, Flatten
+from keras.applications import MobileNetV2  # Import InceptionResNetV2
 from keras.callbacks import EarlyStopping
 from datetime import datetime
 
 def create_model() -> str:
-       # Load the data
-       (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    # Load the data
+    (x_train, y_train), (_, _) = cifar10.load_data()
 
-       # Reshape the data
-       x_train = x_train.reshape(50000, 32, 32, 3)
-       x_test = x_test.reshape(10000, 32, 32, 3)
+    # Reshape and normalize the data
+    x_train = x_train.astype('float32') / 255.0
 
-       # Normalize the data
-       x_train = x_train / 255
-       x_test = x_test / 255
+    # One-hot encode the labels
+    y_train = tf.keras.utils.to_categorical(y_train, 10)
 
-       # One-hot encode the labels
-       y_train = tf.keras.utils.to_categorical(y_train, 10)
-       y_test = tf.keras.utils.to_categorical(y_test, 10)
+    # Create the InceptionResNetV2 base model (pre-trained on ImageNet)
+    base_model = MobileNetV2(weights='imagenet', include_top=False, input_shape=(32, 32, 3))
+    
+    # Freeze the layers of the base model
+    
 
-       model : Sequential = Sequential([InputLayer(input_shape=(32, 32, 3)),
-              Conv2D(filters=32, kernel_size=3,
-                     strides=(2, 2), activation='relu'),
-              MaxPooling2D(pool_size=(2, 2)),
-              Dropout(0.25),
-              Conv2D(filters=64, kernel_size=3,
-                     strides=(2, 2), activation='relu'),
-              MaxPooling2D(pool_size=(2, 2)),
-              Flatten(),
-              Dense(100, activation='relu'),
-              Dense(10, activation='softmax')])
-       callback = EarlyStopping(monitor='loss', patience=3, min_delta=0.01)
+    # Create a sequential model
+    model = Sequential()
 
-       model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
+    # Add the InceptionResNetV2 base model as the initial layer
+    model.add(base_model)
+    base_model.trainable=False
+    # Flatten the output of the base model
+    model.add(Flatten())
 
-       model.fit(x_train, y_train, batch_size=128, epochs=10, verbose=1, validation_split=0.1, callbacks=[callback])
-       
-       
-       date = datetime.now().strftime("%d-%m-%Y_%H-%M")
-       filename = f"model-{date}.keras"
-       
-       model.save("models/"+filename)
-       
-       return filename
+    # Add a dense layer with 64 nodes and ReLU activation
+    model.add(Dense(64, activation='relu'))
+
+    # Add dropout layer
+    model.add(Dropout(0.25))
+
+    # Add another dense layer with 64 nodes and ReLU activation
+    model.add(Dense(64, activation='relu'))
+
+    # Add dropout layer
+    model.add(Dropout(0.25))
+
+    # Add the output layer with 10 nodes for classification
+    model.add(Dense(10, activation='softmax'))
+
+    # Compile the model
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # Print a summary of the model architecture
+    model.summary()
+
+    # Fit the model
+    callback = EarlyStopping(monitor='loss', patience=3, min_delta=0.01)
+
+    model.fit(x_train, y_train, batch_size=128, epochs=10, verbose=1, validation_split=0.1, callbacks=[callback])
+
+    # Save the model
+    date = datetime.now().strftime("%d-%m-%Y_%H-%M")
+    filename = f"model-{date}.keras"
+    model.save("models/" + filename)
+
+    return filename
+
+# Call the function to create and train the model
+create_model()
